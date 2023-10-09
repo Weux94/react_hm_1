8cfb0ed065f47c945dfcee87ec39dce9e47f9c46
Your Name <yehor.litvinov@gmail.com>
2023-10-09 19:24:13 +0300

    Everything is ok&

--- src/componets/Table/Table.js
+++ src/componets/Table/Table.js
@@ -1,18 +1,71 @@
-
 import "./Table.css";
 import { LuArrowUpDown } from "react-icons/lu";
-const Table = () => {
+import { MdOutlineEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
+
+const Table = (props) => {
   return (
-      <tr className="myTable">
-        <th>ID<span className="ico"> <LuArrowUpDown /></span> </th>
-        <th>Category<span className="ico"><LuArrowUpDown /></span> </th>
-        <th>Name<span className="ico"><LuArrowUpDown /></span> </th>
-        <th>Quantity<span className="ico"><LuArrowUpDown /></span> </th>
-        <th>Price (₴) <span className="ico"><LuArrowUpDown /></span></th>
-        <th>Nothing<span className="ico"><LuArrowUpDown /></span> </th>
-      </tr>
+    <table>
+      <thead>
+        <tr className="first-row">
+          <th>
+            ID
+            <span className="upDown">
+              <LuArrowUpDown />
+            </span>
+          </th>
+          <th>
+            Category
+            <span className="upDown">
+              <LuArrowUpDown />
+            </span>
+          </th>
+          <th>
+            Name
+            <span className="upDown">
+              <LuArrowUpDown />
+            </span>
+          </th>
+          <th>
+            Quantity
+            <span className="upDown">
+              <LuArrowUpDown />
+            </span>
+          </th>
+          <th>
+            Price (₴)
+            <span className="upDown">
+              <LuArrowUpDown />
+            </span>
+          </th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {props.data.map((item) => {
+          return (
+            <tr key={item.id} className="myRow">
+              <td>{item.id}</td>
+              <td>{item.category}</td>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price}</td>
+              <td>
+                {
+                  <span className="edit">
+                    <MdOutlineEdit />
+                  </span>
+                }
+                <span className="delete">{<MdDelete />}</span>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
 export default Table;
-
--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -5,26 +5,51 @@ import Title from "../../componets/Title/Title";
 import { BsPerson } from "react-icons/bs";
 import { BsPlus } from "react-icons/bs";
 
+
 import Table from "../../componets/Table/Table";
-import Row from "../../componets/TableRows/Row";
+
+const productsData = [
+  {
+    id: 1,
+    category: "PC",
+    name: "Asus",
+    quantity: 5,
+    price: 25000,
+    nothing: " 1",
+  },
+  {
+    id: 2,
+    category: "Tablet",
+    name: "Apple",
+    quantity: 12,
+    price: 22000,
+    nothing: "Something 1",
+  },
+  {
+    id: 3,
+    category: "TV",
+    name: "Samsung",
+    quantity: 4,
+    price: 18000,
+    nothing: "Something 1",
+  },
+];
+
+
+
 const Products = () => {
   return (
     <div className="body-bcg">
       <LogoWhite />
       <div className="link">
-    
-        <Link text={"Preview"} ico={ <BsPerson /> }/>
+        <Link text={"Preview"} ico={<BsPerson />} />
         <Link text={"Add product"} ico={<BsPlus />} />
       </div>
-      <Title />
-      <table>
-        <thead>
-          <Table />
-        </thead>
-        <tbody>
-          <Row />
-        </tbody>
-      </table>
+      <Title  />
+      <Table data={productsData} />
+     
+      
+     
     </div>
   );
 };
